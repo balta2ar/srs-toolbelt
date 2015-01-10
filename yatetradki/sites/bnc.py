51e0dfecd4ad896bb23930ad5f5a4c65e6b0c4f2
Yuri Bochkarev <baltazar.bz@gmail.com>
2015-01-10 14:11:08 +0300

    Remove first two words from BNC usages (those are kinda useless index)

--- yatetradki/sites/bnc.py
+++ yatetradki/sites/bnc.py
@@ -1,3 +1,4 @@
+from re import sub
 from bs4 import BeautifulSoup
 from requests import Session
 from collections import namedtuple
@@ -17,12 +18,16 @@ class BncSimpleSearch(object):
     def __init__(self):
         self._session = Session()
 
+    def _remove_first_two_words(self, text):
+        return sub(r'^\W*\w+\W+\w+\W*', '', text)
+
     def find(self, word):
         responce = self._session.get(URL_BNC.format(word))
         soup = BeautifulSoup(responce.content)
         start = soup.find('div', {'id': 'solutions'})
         usages = start.findAll('p')[1:]
-        usages = [text_cleanup(x.text) for x in usages]
+        usages = [self._remove_first_two_words(text_cleanup(x.text))
+                  for x in usages]
         usages = sorted(usages, key=len)
         usages = usages or self._DUMMY
         return BncWord(usages)
