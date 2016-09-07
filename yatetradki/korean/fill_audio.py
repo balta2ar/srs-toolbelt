601a69274490aa013866ff0b1bcedcb56ca63dff
Yuri Bochkarev <baltazar.bz@gmail.com>
2016-09-07 13:46:02 +0300

    Add support for --dry-run, --num and word table ripped from hosgeldi.com

--- yatetradki/korean/fill_audio.py
+++ yatetradki/korean/fill_audio.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 """
 This scripts updates my deck of korean words with audios grabbed from
 koreanclass101.com/korean-dictionary. These pronunciations are far
@@ -10,6 +11,9 @@ from shutil import copy2
 from operator import itemgetter
 import codecs
 import logging
+import argparse
+from collections import namedtuple
+from pprint import pformat
 
 from anki import Collection
 
@@ -18,17 +22,38 @@ FORMAT = '%(asctime)-15s %(levelname)s %(message)s'
 logging.basicConfig(format=FORMAT, level=logging.DEBUG)
 
 
-MP3_DIR = '/mnt/video/rip/koreanclass101/dictionary-mp3'
+KOREAN_CLASS_MP3_DIR = '/mnt/video/rip/koreanclass101/dictionary-mp3'
+HOSGELDI_MP3_DIR = '/mnt/video/rip/hosgeldi.com/mp3s'
 MEDIA_DIR = '/home/bz/Documents/Anki/bz/collection.media'
-COPIED_PREFIX = 'kc101_'
-LOOKUP_TABLE_FILENAME = '/mnt/big_ext4/btsync/prg/koreanclass101-dictionary/sort-uniq-table.txt'
+KOREAN_CLASS_COPIED_PREFIX = 'kc101_'
+HOSGELDI_COPIED_PREFIX = 'hosgeldi_'
+KOREAN_CLASS_LOOKUP_TABLE_FILENAME = '/mnt/big_ext4/btsync/prg/koreanclass101-dictionary/sort-uniq-table.txt'
+HOSGELDI_LOOKUP_TABLE_FILENAME = '/mnt/video/rip/hosgeldi.com/uniq.txt'
 WORD_FIELD = 'Korean'
+ENGLISH_FIELD = 'English'
 AUDIO_FIELD = 'Audio'
 MAX_AUDIO_COUNT = 3
 
 
+TableEntry = namedtuple('TableEntry', 'mp3from mp3to mp3tobase')
+
+
 class WordTable(object):
-    def __init__(self, filename):
+    def _make_entry(self, entry_dict):
+        mp3from = join(self._mp3dir, entry_dict['mp3base'])
+        mp3tobase = self._copied_prefix + entry_dict['mp3base']
+        mp3to = join(self._media_dir, mp3tobase)
+        return TableEntry(mp3from, mp3to, mp3tobase)
+
+
+class KoreanClass101WordTable(WordTable):
+    def __init__(self, filename, mp3dir, media_dir, copied_prefix):
+        super(KoreanClass101WordTable, self).__init__()
+
+        self._mp3dir = mp3dir
+        self._media_dir = media_dir
+        self._copied_prefix = copied_prefix
+
         self._db = []
         for line in codecs.open(filename, 'r', encoding='utf-8').readlines():
             line = line.strip()
@@ -49,36 +74,97 @@ class WordTable(object):
                    if entry['korean'] == value
                    and '_' not in entry['mp3base']]
 
-        uniq_sizes = {getsize(join(MP3_DIR, entry['mp3base'])): entry
+        uniq_sizes = {getsize(join(self._mp3dir, entry['mp3base'])): entry
                       for entry in results}
 
         uniq_results = list(uniq_sizes.values())
 
-        return sorted(uniq_results,
-                      key=itemgetter('mp3base'))
+        return [self._make_entry(x)
+                for x in sorted(uniq_results, key=itemgetter('mp3base'))]
+
+
+class HosgeldiWordTable(WordTable):
+    def __init__(self, filename, mp3dir, media_dir, copied_prefix):
+        super(HosgeldiWordTable, self).__init__()
+
+        self._mp3dir = mp3dir
+        self._media_dir = media_dir
+        self._copied_prefix = copied_prefix
+
+        self._db = []
+        for line in codecs.open(filename, 'r', encoding='utf-8').readlines():
+            line = line.strip()
+            try:
+                korean, russian, id, mp3base, mp3url, image_base, image_url = line.split('\t')
+                self._db.append({'korean': '%s' % korean,
+                                 'russian': '%s' % russian,
+                                 'id': id,
+                                 'mp3base': mp3base,
+                                 'mp3url': mp3url,
+                                 'image_base': image_base,
+                                 'image_url': image_url})
+            except ValueError:
+                logging.error('Skipping line: "%s"', line)
+        logging.info('Loaded %d entries from table %s',
+                     len(self._db), filename)
+
+    def lookup(self, value):
+        results = [entry for entry in self._db
+                   if entry['korean'] == value]
+
+        uniq_sizes = {getsize(join(self._mp3dir, entry['mp3base'])): entry
+                      for entry in results}
+
+        uniq_results = list(uniq_sizes.values())
+
+        return [self._make_entry(x)
+                for x in sorted(uniq_results, key=itemgetter('mp3base'))]
 
 
 def main():
+    parser = argparse.ArgumentParser(description='Add audio pronunciation to korean words')
+    parser.add_argument('--dry-run', action='store_true', default=False,
+                        help='Do not modify anything')
+    parser.add_argument('--num', type=int, default=None,
+                        help='Number of items to update')
+    parser.add_argument('--collection', type=str, required=True,
+                        help='Filename of the collection to update (collection.anki2)')
+    parser.add_argument('--model-name', type=str, required=True,
+                        help='Model name to update (CourseraKoreanBasic)')
+    parser.add_argument('--deck-name', type=str, required=True,
+                        help='Deck name to update (korean::coursera-korean)')
+    args = parser.parse_args()
+
     logging.info('Starting')
+    logging.info(pformat(args.__dict__))
+    if args.dry_run:
+        logging.info('Dry run mode')
 
-    table = WordTable(LOOKUP_TABLE_FILENAME)
+    korean_class_table = KoreanClass101WordTable(
+        KOREAN_CLASS_LOOKUP_TABLE_FILENAME,
+        KOREAN_CLASS_MP3_DIR,
+        MEDIA_DIR,
+        KOREAN_CLASS_COPIED_PREFIX
+    )
+    hosgeldi_table = HosgeldiWordTable(
+        HOSGELDI_LOOKUP_TABLE_FILENAME,
+        HOSGELDI_MP3_DIR,
+        MEDIA_DIR,
+        HOSGELDI_COPIED_PREFIX
+    )
 
-    colname = 'collection.anki2'
-    modelname = 'CourseraKoreanBasic'
-    deckname = 'korean::coursera-korean'
-
-    col = Collection(colname)
-    deckid = col.decks.id(deckname)
+    col = Collection(args.collection)
+    deckid = col.decks.id(args.deck_name)
     col.decks.select(deckid)
 
-    model = col.models.byName(modelname)
+    model = col.models.byName(args.model_name)
     deck = col.decks.get(deckid)
     deck['mid'] = model['id']
     col.decks.save(deck)
 
-    cardids = col.findCards('deck:"%s"' % deckname)
-    #noteids = col.findNotes('deck:"%s"' % deckname)
-    logging.info('Found %d cards in "%s" deck', len(cardids), deckname)
+    cardids = col.findCards('deck:"%s"' % args.deck_name)
+    #noteids = col.findNotes('deck:"%s"' % args.deck_name)
+    logging.info('Found %d cards in "%s" deck', len(cardids), args.deck_name)
 
     # from ipdb import set_trace; set_trace()
     # return
@@ -86,51 +172,56 @@ def main():
     updated_count = 0
 
     #for noteid in noteids:
-    for cardid in cardids:
+    for i, cardid in enumerate(cardids):
         card = col.getCard(cardid)
         note = card.note()
         #note = col.getNote(noteid)
         word = note[WORD_FIELD]
+        # word = '의사'
 
+        # en_word = note[ENGLISH_FIELD]
         # strip <div> tags
-        # if word.startswith('<div>'):
-        #     word = word[5:]
-        # if word.endswith('</div>'):
-        #     word = word[:word.rfind('</div>')]
-        # note[WORD_FIELD] = word
+        # if en_word.startswith('<div>'):
+        #     en_word = en_word[5:]
+        # if en_word.endswith('</div>'):
+        #     en_word = en_word[:en_word.rfind('</div>')]
+        # note[ENGLISH_FIELD] = en_word
         # note.flush()
 
-        logging.info('Another card: "%s" "%s" "%s"', cardid, word, note[AUDIO_FIELD])
+        logging.info('Another card: id:"%s" Ko:"%s" audio:"%s" En:"%s"',
+                     cardid, word, note[AUDIO_FIELD], note[ENGLISH_FIELD])
 
         new_audio_field = ''
-        entries = table.lookup(word)[:MAX_AUDIO_COUNT]
+        entries = korean_class_table.lookup(word)[:MAX_AUDIO_COUNT]
+        entries += hosgeldi_table.lookup(word)[:MAX_AUDIO_COUNT]
         for entry in entries:
             logging.info('Entry: "%s"', entry)
-            mp3from = join(MP3_DIR, entry['mp3base'])
-            mp3tobase = COPIED_PREFIX + entry['mp3base']
-            mp3to = join(MEDIA_DIR, mp3tobase)
 
-            if not exists(mp3to):
-                logging.info('Copying "%s" to "%s"', mp3from, mp3to)
-                copy2(mp3from, mp3to)
+            if not args.dry_run and not exists(entry.mp3to):
+                logging.info('Copying "%s" to "%s"', entry.mp3from, entry.mp3to)
+                copy2(entry.mp3from, entry.mp3to)
 
-            new_audio_field += '[sound:%s]' % mp3tobase
-
+            new_audio_field += '[sound:%s]' % entry.mp3tobase
 
         if entries:
             logging.info('New audio field: "%s"', new_audio_field)
             note[AUDIO_FIELD] = new_audio_field
-            note.flush()
+            if not args.dry_run:
+                note.flush()
             updated_count += 1
 
         # from ipdb import set_trace; set_trace()
         # break
+        if args.num is not None and i >= args.num:
+            break
 
     logging.info('Closing DB')
-    col.save()
+    if not args.dry_run:
+        col.save()
     col.close()
 
-    logging.info('Updated %d cards', updated_count)
+    logging.info('Would have updated %d cards' if args.dry_run else 'Updated %d cards',
+                 updated_count)
 
 
 if __name__ == '__main__':
