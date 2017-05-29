67f8bcdc7d7f395d6f4f67597b28251a549fd6e8
Yuri Bochkarev <baltazar.bz@gmail.com>
2017-05-29 15:10:28 +0300

    initial

---
+++ memrise_client.js
@@ -0,0 +1,223 @@
+// ==UserScript==
+// @name         Upload audio to Memrise
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Automatically finds words without audios in memrise course and uploads the audio
+// @author       balta2ar
+// @match        https://www.memrise.com/course/1344980/baltazar-korean-words/edit/*
+// @grant        GM_xmlhttpRequest
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function callAjax(url, callback){
+        //alert("START");
+
+        GM_xmlhttpRequest({
+            method: "GET",
+            url: url,
+            synchronous: true,
+            onload: function(response) {
+                //alert('RECEIVED' + response.responseText);
+                callback(JSON.parse(response.responseText));
+            },
+            onerror: function(response) {
+                var msg = "An error occurred." +
+                    "\nresponseText: " + response.responseText +
+                    "\nreadyState: " + response.readyState +
+                    "\nresponseHeaders: " + response.responseHeaders +
+                    "\nstatus: " + response.status +
+                    "\nstatusText: " + response.statusText +
+                    "\nfinalUrl: " + response.finalUrl;
+                console.error(msg);
+            },
+        });
+        //alert("END");
+    }
+
+    function getAudioForWord(word, onsuccess) {
+        callAjax("http://localhost:5000/api/get_audio/" + encodeURIComponent(word), onsuccess);
+    }
+
+    function resultToBlob(result) {
+        //window.RESULT = result;
+        MEMRISE.RESULT = result;
+        //alert(result['base64_data']);
+        var byteString = atob(result.base64_data);
+        var mimeString = 'audio/mp3';
+
+        var ab = new ArrayBuffer(byteString.length);
+        var ia = new Uint8Array(ab);
+        for (var i = 0; i < byteString.length; i++)
+        {
+            ia[i] = byteString.charCodeAt(i);
+        }
+
+        var bb = new Blob([ab], { "type": mimeString });
+        MEMRISE.byteString = byteString;
+        MEMRISE.ab = ab;
+        MEMRISE.ia = ia;
+        MEMRISE.bb = bb;
+        return bb;
+    }
+
+    //function displayResult(result) {
+    function uploadAudio(result, uploadFormParams, onsuccess) {
+        // https://www.memrise.com/ajax/thing/cell/upload_file/
+
+        //alert('DISPLAY: ' + result);
+
+        /*
+        ------WebKitFormBoundary7wGHsJmxhEeD9hSN
+        Content-Disposition: form-data; name="thing_id"
+
+        129422029
+        ------WebKitFormBoundary7wGHsJmxhEeD9hSN
+        Content-Disposition: form-data; name="cell_id"
+
+        3
+        ------WebKitFormBoundary7wGHsJmxhEeD9hSN
+        Content-Disposition: form-data; name="cell_type"
+
+        column
+        ------WebKitFormBoundary7wGHsJmxhEeD9hSN
+        Content-Disposition: form-data; name="csrfmiddlewaretoken"
+
+        ErZhxGeMIZv3RAoapqUhoezixqgdaRo8EvYlMJIiH8rAfTn1po00DvJBiYGSswfB
+        ------WebKitFormBoundary7wGHsJmxhEeD9hSN
+        Content-Disposition: form-data; name="f"; filename="translate_tts.mp3"
+        Content-Type: audio/mp3
+        */
+
+        var query = {
+            method: "POST",
+            url: "https://www.memrise.com/ajax/thing/cell/upload_file/",
+            onload: function (response) {
+                //alert('UPLOAD RESULT:' + res.responseText);
+                onsuccess(response.responseText);
+            },
+        };
+
+        //var params = {
+        //    thing_id: "query",
+        //    cell_id: "3",
+        //    cell_type: "column",
+        //    csrfmiddlewaretoken: MEMRIZE.csrftoken,
+        //};
+
+        query.data = new FormData();
+        for (var p in uploadFormParams) {
+            query.data.append(p, uploadFormParams[p]);
+        }
+
+        //var formData = new FormData();
+        var blob = resultToBlob(result);
+        query.data.append('f', blob, 'sound.mp3');
+        //formData.append('f', blob, 'sound.mp3');
+
+        //query.headers = {"Content-Type": "multipart/form-data"};
+
+        GM_xmlhttpRequest(query);
+
+        //alert('DISPLAY DONE');
+    }
+
+    function onUploadedSuccessfully(result, thing, buttons) {
+        buttons.innerHTML = 'UPLOADED';
+    }
+
+    function onWordNotFound(result, thing, buttons) {
+        buttons.innerHTML = 'NOT FOUND';
+    }
+
+    function onAddAudio(word, uploadFormParams, thing, buttons) {
+        //alert("ADDING AUDIO FOR WORD " + word);
+        //callAjax("http://localhost:5000/api/get_audio/" + encodeURIComponent(word), displayResult);
+        getAudioForWord(word, function(result) {
+            if (result.success) {
+                uploadAudio(result, uploadFormParams, function(uploadResult) {
+                    onUploadedSuccessfully(uploadResult, thing, buttons);
+                });
+            } else {
+                console.error('Cound not find audio for word: ' + word);
+                onWordNotFound(result, thing, buttons);
+            }
+        });
+    }
+
+    function addStartButton() {
+        var header = document.querySelector('ul.header-nav');
+        // <li class="header-nav-item plain ">
+        // <a href="/home/" class="nav-item-btn">
+        //            <span class="nav-item-btn-text">Home</span>
+        var startButton = document.createElement('li');
+        startButton.setAttribute('class', 'header-nav-item plain');
+        //startButton.innerHTML = 'Add missing audio [bz]';
+        startButton.innerHTML = '<a class="nav-item-btn"> <span class="nav-item-btn-text">Add missing audio [bz]</span> </a>';
+        startButton.addEventListener('click', function(event) { findMissingAndUpload(); } );
+        header.appendChild(startButton);
+    }
+
+    function uploadForThing(thing) {
+        var buttons = thing.querySelector('button.btn.btn-mini.dropdown-toggle.disabled');
+
+        if (buttons === null) {
+            return;
+        }
+
+        var buttonGroup = buttons.parentNode;
+
+        var firstElement = buttons.parentNode.parentNode.parentNode.getElementsByClassName('text')[0];
+        var word = firstElement.innerText.trim();
+        var thingId = thing.getAttribute('data-thing-id');
+
+        var uploadFormParams = {
+            thing_id: thingId,
+            cell_id: "3",
+            cell_type: "column",
+            csrfmiddlewaretoken: MEMRISE.csrftoken,
+        };
+
+        var customUploadButton = document.createElement('div');
+        //<div class="btn btn-mini open-recorder">Record</div>
+        customUploadButton.setAttribute('class', 'btn btn-mini');
+        //customUploadButton.setAttribute('onclick', 'onAddAudio(' + word + ')');
+        customUploadButton.addEventListener('click', function(event) {
+            onAddAudio(word, uploadFormParams, thing, buttons);
+        });
+        onAddAudio(word, uploadFormParams, thing, buttons);
+        customUploadButton.innerHTML = 'ADDAUDIO';
+
+        buttonGroup.appendChild(customUploadButton);
+
+        //var upload = buttons.parentNode.children[0]
+
+        //var first = buttons;
+        //var row = first.parentElement.parentElement.parentElement;
+        //var word = row.children[1].innerText.trim();
+
+        //callAjax("http://localhost:5000/api/get_audio/" + encodeURIComponent(word), displayResult);
+
+        //alert(word);
+    }
+
+    function findMissingAndUpload() {
+        //alert("HELLO");
+        //var things = document.querySelector('tbody', 'things');
+
+        var things = document.getElementsByClassName('thing');
+        for (var i = 0; i < things.length; i++) {
+
+            if (i >= 9999) {
+                break;
+            }
+
+            var thing = things[i];
+            uploadForThing(thing);
+        }
+    }
+
+    //setTimeout(function() { findAndUpload(); }, 5000);
+    setTimeout(function() { addStartButton(); }, 1000);
+})();
---
+++ memrise_server.py
@@ -0,0 +1,68 @@
+"""
+This is backend server for memrise_client.js.
+
+Run this server before using memrise_client.js in a browser. The client
+will call API to get audio for words and will upload them to memrise
+servers.
+
+This server is using fill_audio module that references local tables
+with words/audios.
+"""
+import logging
+try:
+    from urllib.parse import unquote
+except ImportError:
+    from urllib import unquote
+from base64 import b64encode
+
+from flask import Flask, jsonify
+
+from fill_audio import create_master_table
+
+
+FORMAT = '%(asctime)-15s %(levelname)s %(message)s'
+logging.basicConfig(format=FORMAT, level=logging.DEBUG)
+
+app = Flask(__name__)
+MASTER_TABLE = create_master_table()
+
+
+@app.route("/")
+def root():
+    logging.info('Root')
+    return "Root"
+
+
+def slurp(filename):
+    with open(filename, 'rb') as file_:
+        return file_.read()
+
+
+@app.route("/api/get_audio/<string:word>")
+def get_audio(word):
+    word = unquote(word)
+    #return 'WORD: %s' % word
+    results = MASTER_TABLE.lookup(word)
+    if results:
+        data = slurp('sample.mp3')
+        #data = slurp(results[0].mp3from)
+        base64 = b64encode(data).decode('utf-8')
+        result = {
+            'success': True,
+            'word': word,
+            'base64_data': base64,
+        }
+        #logging.info('word: %s', word)
+        logging.info('word size (bytes): %s, %s', word, len(data))
+        return jsonify(result)
+    else:
+        result = {
+            'success': False,
+            'word': word,
+        }
+        logging.info('word not found: %s', word)
+        return jsonify(result)
+
+
+if __name__ == '__main__':
+    app.run()
