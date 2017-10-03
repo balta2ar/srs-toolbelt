599cda2af2cf8a253cdd7ab05870cb4fe04b18c1
Yuri Bochkarev <baltazar.bz@gmail.com>
2017-10-03 22:55:03 +0300

    korean to tsv converter (basic)

--- yatetradki/tools/korean_to_tsv.py
+++ yatetradki/tools/korean_to_tsv.py
@@ -20,3 +20,43 @@ Back: Предприниматель. Зубная паста.
 
 """
 
+import sys
+import re
+
+
+RE_HANGUL = re.compile('.*[ㄱ-힣].*')
+
+
+def is_hangul(text):
+    return RE_HANGUL.match(text) is not None
+
+
+def convert_file(filename):
+    pairs = []
+    with open(filename) as file_:
+        left, right = '', ''
+        for line in file_:
+            line = line.strip()
+            if not line:
+                if left or right:
+                    pairs.append('%s\t%s' % (left, right))
+                    left, right = '', ''
+                continue
+
+            if is_hangul(line):
+                left += '<p>' + line + '</p>'
+            else:
+                right += '<p>' + line + '</p>'
+            # print('HANGUL', line, is_hangul(line))
+        if left or right:
+            pairs.append('%s\t%s' % (left, right))
+    return '\n'.join(pairs)
+
+
+def main(args):
+    for filename in args:
+        print(convert_file(filename))
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
