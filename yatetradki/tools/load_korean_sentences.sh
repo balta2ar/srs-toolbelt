73813dc5439af0950ed215a6fb57c6f37ae8724d
Yuri Bochkarev <baltazar.bz@gmail.com>
2017-10-11 22:14:34 +0300

    load words into sejong and wongwan decks

--- yatetradki/tools/load_korean_sentences.sh
+++ yatetradki/tools/load_korean_sentences.sh
@@ -6,28 +6,37 @@
 
 export PATH="$HOME/bin:$PATH"
 
-TXT=$1
-TSV=$2
-
+# TXT=$1
+# TSV=$2
+#
 SRS_TOOLBELT_ROOT=/mnt/data/prg/src/bz/python/yandex-slovari-tetradki
+KEEP=/mnt/data/prg/src/bz/python/sync_google_keep/sync_google_keep_notes
 
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <KoreanSentences.txt> <korean-sentences.tsv>"
-    exit 1
-fi
+# if [ "$#" -ne 2 ]; then
+#     echo "Usage: $0 <KoreanSentences.txt> <korean-sentences.tsv>"
+#     exit 1
+# fi
 
 convert() {
+    TXT=$1
+    TSV=$2
     python $SRS_TOOLBELT_ROOT/yatetradki/tools/korean_to_tsv.py $TXT > $TSV
 }
 
 update() {
+    TSV=$1
+    DECK=$2
     PYTHONPATH=$SRS_TOOLBELT_ROOT \
         python2 $SRS_TOOLBELT_ROOT/yatetradki/tools/load_from_csv.py \
-            --deck 'korean::korean-sentences' \
+            --deck $DECK \
             --model 'KoreanSentences' \
             --fields 'Word,Example,Description,Audio' \
             --csv $TSV --update
 }
 
-convert
-update
+convert "$KEEP/KoreanSentencesSejong.txt" 'korean-sentences-sejong.tsv'
+update 'korean-sentences-sejong.tsv' 'korean::korean-sentences::-sejong'
+
+convert "$KEEP/KoreanSentencesWongwan.txt" 'korean-sentences-wongwan.tsv'
+update 'korean-sentences-wongwan.tsv' 'korean::korean-sentences::~wongwan'
+
