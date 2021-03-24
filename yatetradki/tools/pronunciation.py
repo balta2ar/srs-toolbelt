d7f58fd99d076bb555924379471016207d8fe70f
Yuri Bochkarev <baltazar.bz@gmail.com>
2021-03-24 21:03:20 +0100

    audio: add add_audio.py script (srst-add-audio) that adds norwegian pronunciation to fields that miss audio

---
+++ yatetradki/tools/pronunciation.py
@@ -0,0 +1,66 @@
+from os.path import basename
+
+from yatetradki.korean.fill_audio import CustomServiceWithFunction
+from yatetradki.korean.fill_audio import create_cached_table
+from yatetradki.korean.fill_audio import create_norwegian_table
+from yatetradki.tools.audio import get_pronunciation_call
+from yatetradki.tools.log import get_logger
+
+NORWEGIAN_PRONUNCIATION_TABLE = create_norwegian_table()
+
+
+_logger = get_logger('pronunciation')
+
+
+class Pronunciation:
+    def __init__(self, audio_type):
+        self._audio_type = audio_type
+    def fill(self, word, col, fields):
+        return fill_pronunciation(self._audio_type, word, col, fields)
+
+
+def get_pronunciation_implementation(audio_type):
+    PRONUNCIATION_TABLE = {
+        'norwegian': get_norwegian_pronunciation,
+        'english': get_english_pronunciation,
+        #'english': get_pronunciation_call,
+    }
+    return PRONUNCIATION_TABLE.get(audio_type)
+
+
+def get_norwegian_pronunciation(word):
+    """Should return a filename (mp3) with the pronounced word. None if not found."""
+    results = NORWEGIAN_PRONUNCIATION_TABLE.lookup(word)
+    print(results)
+    if not results:
+        return None
+    return results[0].mp3from
+
+
+def get_english_pronunciation(word):
+    """Should return a filename (mp3) with the pronounced word. None if not found."""
+    cache_dir = 'cache_english_awesometts'
+    prefix = 'cache_english_awesometts_'
+    english_service = CustomServiceWithFunction(prefix, get_pronunciation_call)
+    results = create_cached_table(cache_dir, prefix, english_service).lookup(word)
+    print(results)
+    if not results:
+        return None
+    return results[0].mp3from
+
+
+def fill_pronunciation(audio_type, word, col, fields):
+    get_pronunciation = get_pronunciation_implementation(audio_type)
+    if get_pronunciation is None:
+        _logger.warning('Pronunciation "%s" is not supported', audio_type)
+        return False
+
+    audio = get_pronunciation(word)
+    if audio is None:
+        _logger.warning('Could not add audio for word %s', word)
+    else:
+        _logger.info('Adding audio for word %s: %s', word, audio)
+        col.media.addFile(audio)
+        fields['Audio'] = '[sound:%s]' % basename(audio)
+    return True
+
