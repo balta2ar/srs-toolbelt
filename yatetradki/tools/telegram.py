0a4ae3dcba44e18bab7a1eb8e4349e7b4fe1d6fd
Yuri Bochkarev <baltazar.bz@gmail.com>
2021-04-03 14:02:35 +0200

    notify telegram: quote message

--- yatetradki/tools/telegram.py
+++ yatetradki/tools/telegram.py
@@ -1,7 +1,9 @@
 import logging
-from yatetradki.utils import must_env
+from urllib.parse import quote
 
 import requests
+
+from yatetradki.utils import must_env
 
 logging.getLogger("requests").setLevel(logging.WARNING)
 
@@ -11,6 +13,7 @@ CHAT_ID = must_env('TELEGRAM_CHAT_ID')
 
 
 def notify(message):
+    message = quote(message)
     data = "chat_id={0}&text={1}".format(CHAT_ID, message)
     url = "https://api.telegram.org/bot{0}/sendMessage?{1}".format(ACCESS_TOKEN, data)
     response = requests.post(url, data)
