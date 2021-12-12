f3f9c614f0df8cd5de5c6819dad657ca71da537b
Yuri Bochkarev <baltazar.bz@gmail.com>
2021-12-12 10:47:31 +0100

    textmarksman: single column mode on ocr, strip consecutive newlines

--- yatetradki/uitools/textmarksman/textmarksman.py
+++ yatetradki/uitools/textmarksman/textmarksman.py
@@ -6,6 +6,7 @@ import argparse
 import subprocess
 from typing import Optional
 import tesserocr
+from tesserocr import PSM, OEM
 import pyperclip
 
 from yatetradki.uitools.sayit.sayit import sayit
@@ -21,7 +22,26 @@ def capture() -> Optional[str]:
     return None
 
 def ocr(filename: str, lang: str) -> str:
-    with tesserocr.PyTessBaseAPI(lang=lang) as api:
+    # https://github.com/sirfz/tesserocr/blob/master/tesseract.pxd#L293
+    # OSD_ONLY,                # Orientation and script detection only.
+    # AUTO_OSD,                # Automatic page segmentation with orientation and
+    #                          # script detection. (OSD)
+    # AUTO_ONLY,               # Automatic page segmentation, but no OSD, or OCR.
+    # AUTO,                    # Fully automatic page segmentation, but no OSD.
+    # SINGLE_COLUMN,           # Assume a single column of text of variable sizes.
+    # SINGLE_BLOCK_VERT_TEXT,  # Assume a single uniform block of vertically
+    #                          # aligned text.
+    # SINGLE_BLOCK,            # Assume a single uniform block of text. (Default.)
+    # SINGLE_LINE,             # Treat the image as a single text line.
+    # SINGLE_WORD,             # Treat the image as a single word.
+    # CIRCLE_WORD,             # Treat the image as a single word in a circle.
+    # SINGLE_CHAR,             # Treat the image as a single character.
+    # SPARSE_TEXT,             # Find as much text as possible in no particular order.
+    # SPARSE_TEXT_OSD,         # Sparse text with orientation and script det.
+    # RAW_LINE,                # Treat the image as a single text line, bypassing
+    #                          # hacks that are Tesseract-specific.
+    # COUNT                    # Number of enum entries.
+    with tesserocr.PyTessBaseAPI(lang=lang, psm=PSM.SINGLE_COLUMN) as api:
         api.SetImageFile(filename)
         return api.GetUTF8Text()
 
@@ -37,6 +57,9 @@ def unwrap(text: str) -> str:
     text = re.sub(expr, r'\1 ', text)
     # collapse spaces
     text = re.sub(r'[ \t]+', ' ', text)
+    #text = re.sub(r'[\n]+', '\n', text)
+    # remove 2 consecutive newlines
+    text = re.sub(r'\n\n', '\n', text)
     return text
 
 def copy(text: str) -> str:
