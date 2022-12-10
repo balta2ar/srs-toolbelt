6dd974f34e986213ba059f492af8a6fa36e84357
Yuri Bochkarev <baltazar.bz@gmail.com>
2022-12-10 17:06:44 +0100

    mindmap: first experiments with basic layouts

---
+++ yatetradki/uitools/mindmap/main.js
@@ -0,0 +1,164 @@
+let main
+window.onload = function () {
+    main = document.getElementById('main')
+    main.innerText = "init..."
+
+    Main()
+}
+
+class Node {
+    constructor(text, children) {
+        this.id = 0
+        this.text = text
+        this.children = children
+    }
+}
+
+function example() {
+    return new Node("norsk", [
+        new Node("liv omstendigheter", [
+            new Node("familie", []),
+            new Node("arbeid", []),
+        ]),
+        new Node("handlinger", [
+            new Node("bevegelse", [
+                new Node("gå", []),
+                new Node("stå", []),
+                new Node("pile ut", []),
+            ]),
+            new Node("slå / kamp", [
+                new Node("dytte", []),
+                new Node("skyve", []),
+            ]),
+        ]),
+        new Node("følelser", [
+            new Node("positive", [
+                new Node("lykke", []),
+                new Node("glede", []),
+            ]),
+            new Node("negative", [
+                new Node("sorg", []),
+                new Node("angst", []),
+                new Node("sin", []),
+            ]),
+        ]),
+    ])
+}
+
+function layoutNaiveDownTree(root, parent, baseX, baseY) {
+    const marginX = 10
+    const marginY = 20
+
+    function scan(node, x, y) {
+        const t = addSvg(parent, 'text', {
+            x: x, y: y, style: ''
+        })
+        t.textContent = node.text
+        var childI = 0
+        var maxR = x
+        for (const child of node.children) {
+            const mx = childI === 0 ? 0 : marginX
+            const [l, r] = scan(child, maxR + mx, y + marginY)
+            maxR = Math.max(maxR, r)
+            childI++
+        }
+        const w = t.getComputedTextLength()
+        return [x, Math.max(x + w, maxR)]
+    }
+
+    scan(root, baseX, baseY)
+}
+
+function layoutNaiveRightTree(root, parent, baseX, baseY) {
+    const marginX = 20
+    const marginY = 10
+
+    function scan(node, x, y) {
+        const t = addSvg(parent, 'text', {
+            x: x, y: y, style: ''
+        })
+        t.textContent = node.text
+        const w = t.getComputedTextLength()
+        var childI = 0
+        var maxB = y
+        for (const child of node.children) {
+            const my = childI === 0 ? 0 : marginY
+            const [t, b] = scan(child, x + w + marginX, maxB + my)
+            maxB = Math.max(maxB, b)
+            childI++
+        }
+
+        return [y, Math.max(y + marginY, maxB)]
+    }
+
+    scan(root, baseX, baseY)
+}
+
+function Main() {
+    main.innerText = ""
+    var svg = addSvg(main, 'svg', {
+        width: "100%",
+        height: "100%",
+        style: 'border:1px solid #000000'
+    })
+
+    var g = addSvg(svg, 'g', {
+        transform: 'translate(10, 20)'
+    })
+
+    // var rect = addSvg(g, 'rect', {
+    //     x: 0,
+    //     y: 0,
+    //     width: 100,
+    //     height: 100,
+    //     style: 'fill: #000000; stroke: #000000; stroke-width: 1'
+    // })
+
+    var text = addSvg(g, 'text', {
+        x: 0,
+        y: 0,
+        style: 'fill: #000000; stroke: #ff0000; stroke-width: 1'
+    })
+
+    text.textContent = 'hello'
+
+    const root = example()
+    console.log('root: %o', root)
+    const g2 = addSvg(svg, 'g', {
+        transform: 'translate(50, 100)'
+    })
+    layoutNaiveRightTree(root, g2, 0, 0)
+    const g3 = addSvg(svg, 'g', {
+        transform: 'translate(400, 100)'
+    })
+    layoutNaiveDownTree(root, g3, 0, 0)
+}
+
+function addSvg(parent, type, attr) {
+    var el = document.createElementNS('http://www.w3.org/2000/svg', type);
+    parent.appendChild(el)
+    forEach(attr, (k, v) => {
+        el.setAttribute(k, v)
+    })
+    return el
+}
+
+function addElement(parent, type, attr) {
+    var el = document.createElement(type);
+    parent.appendChild(el)
+    forEach(attr, (k, v) => {
+        el.setAttribute(k, v)
+    })
+    return el
+}
+
+function forEach(obj, cb) {
+    if (typeof obj != "object") {
+        return
+    }
+    for (const k of Object.keys(obj)) {
+        if (k) {
+            cb(k, obj[k])
+        }
+    }
+}
