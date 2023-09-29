c1724c5d7b39d2b2775598bd1ac4af5961f98a7b
Yuri Bochkarev <baltazar.bz@gmail.com>
2023-09-29 09:41:52 +0200

    harken: commit

--- yatetradki/uitools/harken/harken.py
+++ yatetradki/uitools/harken/harken.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 
 import os
+from aiohttp import web
 from typing import List, Optional
-from fastapi import FastAPI, HTTPException
-import uvicorn
 from pydantic import BaseModel
-import subprocess
 
 
 # Models
@@ -19,10 +17,6 @@ class MediaDetail(BaseModel):
     file_name: str
     file_path: str
     subtitles: List[Subtitle]
-
-
-# FastAPI App
-app = FastAPI()
 
 
 # Helper Function to Parse SRT
@@ -42,20 +36,24 @@ def parse_srt(file_path: str) -> List[Subtitle]:
     return subtitles
 
 
-# API Route to fetch media
-@app.get("/media/{file_name}", response_model=MediaDetail)
-def fetch_media(file_name: str) -> Optional[MediaDetail]:
+# Handler to fetch media
+async def fetch_media(request):
+    file_name = request.match_info.get('file_name', '')
     media_path = os.path.join('media', file_name)
     subtitle_path = os.path.join('subtitles', f"{os.path.splitext(file_name)[0]}.srt")
     
     if not os.path.exists(media_path) or not os.path.exists(subtitle_path):
-        raise HTTPException(status_code=404, detail="Media or Subtitle not found")
+        return web.HTTPNotFound(text="Media or Subtitle not found")
     
     subtitles = parse_srt(subtitle_path)
-    return MediaDetail(file_name=file_name, file_path=media_path, subtitles=subtitles)
+    media_detail = MediaDetail(file_name=file_name, file_path=media_path, subtitles=subtitles)
+    return web.json_response(media_detail.dict())
 
 
-# Run Uvicorn from Python Code
+# aiohttp App
+app = web.Application()
+app.router.add_get('/media/{file_name}', fetch_media)
+
+# Run aiohttp from Python Code
 if __name__ == "__main__":
-    # uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
-    subprocess.run(["uvicorn", __file__.replace(".py", ":app"), "--reload", "--host", "127.0.0.1", "--port", "8000"])
+    web.run_app(app, host="127.0.0.1", port=8000)
