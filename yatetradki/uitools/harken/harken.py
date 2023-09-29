dbd1c71d489136d599dab59254cc89927c7ef841
Yuri Bochkarev <baltazar.bz@gmail.com>
2023-09-29 09:45:35 +0200

    harken: commit

--- yatetradki/uitools/harken/harken.py
+++ yatetradki/uitools/harken/harken.py
@@ -19,8 +19,8 @@ class MediaDetail(BaseModel):
     subtitles: List[Subtitle]
 
 
-# Helper Function to Parse SRT
-def parse_srt(file_path: str) -> List[Subtitle]:
+# Helper Function to Parse VTT
+def parse_vtt(file_path: str) -> List[Subtitle]:
     subtitles = []
     with open(file_path, 'r', encoding='utf-8') as f:
         blocks = f.read().split('\n\n')
@@ -28,9 +28,9 @@ def parse_srt(file_path: str) -> List[Subtitle]:
             if block.strip() == '':
                 continue
             lines = block.split('\n')
-            if len(lines) < 3:
+            if len(lines) < 2:
                 continue  # Invalid block
-            start_end, *text_lines = lines
+            start_end, *text_lines = lines[1:]
             start, end = start_end.split(' --> ')
             subtitles.append(Subtitle(start_time=start, end_time=end, text=' '.join(text_lines)))
     return subtitles
@@ -40,12 +40,12 @@ def parse_srt(file_path: str) -> List[Subtitle]:
 async def fetch_media(request):
     file_name = request.match_info.get('file_name', '')
     media_path = os.path.join('media', file_name)
-    subtitle_path = os.path.join('subtitles', f"{os.path.splitext(file_name)[0]}.srt")
+    subtitle_path = os.path.join('media', f"{os.path.splitext(file_name)[0]}.vtt")
     
     if not os.path.exists(media_path) or not os.path.exists(subtitle_path):
         return web.HTTPNotFound(text="Media or Subtitle not found")
     
-    subtitles = parse_srt(subtitle_path)
+    subtitles = parse_vtt(subtitle_path)
     media_detail = MediaDetail(file_name=file_name, file_path=media_path, subtitles=subtitles)
     return web.json_response(media_detail.dict())
 
