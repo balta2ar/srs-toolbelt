5a43c524472566bd7f99d6925319c219c928aee6
Yuri Bochkarev <baltazar.bz@gmail.com>
2023-10-31 08:40:36 +0100

    compress-audio-bot: load env

--- yatetradki/uitools/compress-audio-bot/compress_audio_bot.py
+++ yatetradki/uitools/compress-audio-bot/compress_audio_bot.py
@@ -6,18 +6,27 @@
 import logging
 import subprocess
 from os import environ
+from os.path import expanduser, expandvars, exists
 from tempfile import TemporaryDirectory
 
 from telegram import Bot, Update
 from telegram.ext import (ApplicationBuilder, CommandHandler, ContextTypes,
                           MessageHandler, Updater, filters)
 
-TOKEN = environ.get('TELEGRAM_COMPRESS_AUDIO_BOT_TOKEN')
-
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 logging.getLogger("httpx").setLevel(logging.WARNING)
-logger = logging.getLogger(__name__)
+
+def load_env(filename):                                                                                                                               
+    filename = expanduser(expandvars(filename))                                                                                                       
+    if not exists(filename):                                                                                                                          
+        logging.warning('Missing env file "%s"', filename)                                                                                            
+        return                                                                                                                                        
+    with open(filename, 'r') as f:                                                                                                                    
+        for line in [x.strip() for x in f.readlines()]:                                                                                               
+            if line.startswith('#'): continue                                                                                                         
+            key, value = line.strip().split('=', 1)                                                                                                   
+            environ[key] = value
 
 
 async def audio_handler(update: Update, context):
@@ -33,13 +42,15 @@ async def audio_handler(update: Update, context):
             "-vbr", "on", "-ar", "16000", "-compression_level", "10",
             "-f", "ogg",
             compressed_path]
-        logger.info('Running command: %s', ' '.join(cmd))
+        logging.info('Running command: %s', ' '.join(cmd))
         subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
         with open(compressed_path, 'rb') as audio:
             await context.bot.send_voice(chat_id=update.effective_message.chat_id, voice=audio)
 
 def main():
+    load_env('~/.telegram')
+    TOKEN = environ.get('TELEGRAM_COMPRESS_AUDIO_BOT_TOKEN')
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(MessageHandler(filters.VOICE, audio_handler))
     app.run_polling()
